2ef1b87228eb4a6b186ce7fe70799b60983082ad
Cristian <cristianlopezr.931@gmail.com>
2025-05-09 13:26:14 -0400

    Added prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import { prisma } from './lib/prisma-client';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
+app.get('/', async (req, res) => {
+    res.status(202).json({
         hello: 'world',
     });
 });
